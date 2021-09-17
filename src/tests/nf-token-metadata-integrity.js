ee4cbf532c292650a37efbc167ee60f520db2de5
Tadej Vengust <vengust.tadej@gmail.com>
2021-09-17 13:32:21 +0200

    add digest generation

--- src/tests/nf-token-metadata-integrity.js
+++ src/tests/nf-token-metadata-integrity.js
@@ -1,11 +1,17 @@
 const { expect } = require('chai');
+const { utils } = require('ethers');
+const schema = require('../meta/schema.json');
+const metadata = require('../meta/metadata.json');
 
 describe('nf-token-metadata-integrity', function() {
-  let nfToken, owner, bob;
-  const schemaDigest = '0x9c22ff5f21f0b81b113e63f7db6da94fedef11b2119b4088b89664fb9a3cb658';
+  let nfToken, owner, bob, schemaDigest, digest1;
   const id1 = 1;
   const uri1 = 'http://example.foo/1';
-  const digest1 = '0x973124ffc4a03e66d6a4458e587d5d6146f71fc57f359c8d516e0b12a50ab0d9';
+
+  before(async () => {
+    schemaDigest = utils.sha256(utils.toUtf8Bytes(JSON.stringify(schema)));
+    digest1 = utils.sha256(utils.toUtf8Bytes(JSON.stringify(metadata)));
+  });
 
   beforeEach(async () => {
     const nftContract = await ethers.getContractFactory('NFTokenMetadataIntegrity');
